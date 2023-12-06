b03a1293ef660cdec813b20e93de105c8754c8c0
Ji Xing <dzmfg@JideMacBook-Pro.local>
2023-12-06 23:24:37 +0800

    Changes to be committed: 	modified:   Cargo.lock modified:   Cargo.toml 	new file:   demo/lib.rs

---
+++ src/basic.rs
@@ -0,0 +1,71 @@
+//定义一些游戏中的基础组件
+use bevy::{prelude::{Resource,Color},time::Timer};
+use bevy::prelude::Component;
+use rand::Rng;
+
+#[derive(Resource,Clone)]
+pub struct  GameState{
+    pub paused: bool, // 是否暂停
+    pub row_count: usize, // 世界的宽度
+    pub density: usize, // 1~9 设置为随机产生细胞分布的概率
+    pub circles: i32 ,//细胞存活周期数
+    pub active_cells:Vec<bool>,
+}
+//ui状态
+#[derive(Resource)]
+pub struct  UiState{
+    pub row_count_input:f32,
+    pub density_input:f32,
+}
+
+impl Default for UiState{
+    fn default() -> Self {
+        Self { row_count_input: 10., density_input: 3. }
+    }
+}
+
+//计时器
+#[derive(Resource)]
+pub struct LifeTimer(pub Timer);
+
+//初始化游戏状态
+impl GameState{
+    pub fn new(density:usize, row_count:usize)->Self {
+        let mut arr=Vec::new();
+        let mut rng= rand::thread_rng();
+        for _ in 0..row_count*row_count{
+            if rng.gen_range(0..10)<density{
+                arr.push(true);
+            }else{ arr.push(false)}
+        }
+        Self{
+            paused:true,
+            row_count,
+            density,
+            circles:0,
+            active_cells:arr,
+        }
+    }
+}
+
+impl Default for GameState{
+    fn default() -> Self {
+        Self::new(3,10)
+    }
+}
+
+//细胞ID
+#[derive(Component)]
+pub struct CellId(pub i32);
+//细胞集合
+#[derive(Component)]
+pub struct  CellSet;
+
+
+pub const ACTIVE_COLOR:Color=Color::rgb(0.2, 0.3, 1.);
+pub const INIT_COLOR:Color=Color::rgb(0.2, 0.2, 0.2);
+pub const UI_HEIGHT:f32 = 40.;
+pub const WINDOW_INIT_HEIGHT:f32=700.;
+pub const WINDOW_INIT_WIDTH:f32=1000.;
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use bevy::prelude::*;
+pub mod basic;
+use basic::*;
+use bevy::{prelude::*, transform::commands};
+use bevy_egui::{egui,EguiContext,EguiPlugin};
 
 fn main() {
     App::new()
-    .add_systems(Update, hello_world)
+    .add_plugins(DefaultPlugins)
+    .add_plugins(EguiPlugin)
+    .insert_resource(LifeTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
+    .init_resource::<GameState>()
+    .init_resource::<UiState>()
+    .add_systems(Startup, setup)
     .run();
 }
-fn hello_world() {
-    println!("hello world!");
+
+/**
+ *  游戏初始化
+ */
+fn setup(mut cmd:Commands,game_state:Res<GameState>,mut windows: Query<&mut Window>,){
+    let mut window=windows.single_mut();
+    window.resolution.set(WINDOW_INIT_WIDTH, WINDOW_INIT_HEIGHT);
+    cmd.spawn(Camera2dBundle::default());
+    
 }
